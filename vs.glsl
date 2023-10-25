5dd5a4f79c5cd993d85183a50c82e4b86e16575e
Tesseract22 <caleb2222@qq.com>
2023-10-24 23:25:14 -0500

    rework rotation

--- vs.glsl
+++ vs.glsl
@@ -2,15 +2,11 @@
 layout(location=0) in vec4 position;
 layout(location=1) in vec4 color;
 layout(location=2) in vec3 normal;
-out vec4 color2;
 out vec3 vnormal;
 out vec3 mvnormal;
 uniform mat4 mv;
-uniform mat4 v;
 uniform mat4 p;
 void main() {
     gl_Position = p * mv * position;
-    color2 = color;
-    vnormal = mat3(v) * normal;
     mvnormal = mat3(mv) * normal;
 }
